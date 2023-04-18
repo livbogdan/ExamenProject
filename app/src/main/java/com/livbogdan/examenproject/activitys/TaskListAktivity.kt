fff8e7dda9ba1bb3bd02991b5f0e8faff26ad311
livbogdan <livbogdan@gmail.com>
2023-04-18 18:10:05 +0200

    Board Creation v0.3
    Complited with creation of board in MainActivity.
    Board can shows in main screen and when pressed going to another activity
    Started with Task List

---
+++ app/src/main/java/com/livbogdan/examenproject/activitys/TaskListAktivity.kt
@@ -0,0 +1,12 @@
+package com.livbogdan.examenproject.activitys
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.livbogdan.examenproject.R
+
+class TaskListAktivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_task_list_aktivity)
+    }
+}
