beca1a378852615457859bdb7fc5f1d896a34fd0
livbogdan <livbogdan@gmail.com>
2023-04-20 01:51:19 +0200

    Task List Update v 0.2
    1) Created visual components for the Task List Activity.
    2) Created integration to FireStore Database
    3) User can create task list

--- app/src/main/java/com/livbogdan/examenproject/activitys/TaskListAktivity.kt
+++ app/src/main/java/com/livbogdan/examenproject/activitys/TaskListAktivity.kt
@@ -2,12 +2,19 @@ package com.livbogdan.examenproject.activitys
 
 import android.os.Bundle
 import androidx.appcompat.widget.Toolbar
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.livbogdan.examenproject.R
+import com.livbogdan.examenproject.adapters.TaskListAdapter
 import com.livbogdan.examenproject.firebase.FirestoreClass
 import com.livbogdan.examenproject.models.Board
+import com.livbogdan.examenproject.models.Task
 import com.livbogdan.examenproject.utils.Constants
 
 class TaskListAktivity : BaseActivity() {
+
+    private lateinit var mBoardDetails: Board
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_task_list_aktivity)
@@ -21,14 +28,14 @@ class TaskListAktivity : BaseActivity() {
         FirestoreClass().getBoardDetails(this, boardDocumentId)
     }
 
-    private fun setupActionBar(toolBarTitle: String) {
+    private fun setupActionBar() {
         val toolbar: Toolbar = findViewById(R.id.tb_task_list_activity)
         setSupportActionBar(toolbar)
 
         supportActionBar?.apply {
             setDisplayHomeAsUpEnabled(true)
             setHomeAsUpIndicator(R.drawable.back_icon)
-            title = toolBarTitle
+            title = mBoardDetails.name
         }
 
         toolbar.setNavigationOnClickListener {
@@ -37,8 +44,40 @@ class TaskListAktivity : BaseActivity() {
     }
 
     fun boardDetails(board: Board){
+        mBoardDetails = board
+
         hideProgressDialog()
-        setupActionBar(board.name)
+        setupActionBar()
+
+        val addTaskList = Task(resources.getString(R.string.add_list))
+        board.taskList.add(addTaskList)
+
+        val rvTaskList: RecyclerView = findViewById(R.id.rv_task_list)
+
+        rvTaskList.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+
+        rvTaskList.setHasFixedSize(true)
+
+        val adapter = TaskListAdapter(this, board.taskList)
+        rvTaskList.adapter = adapter
     }
 
+    fun addUpdateTaskListSuccess(){
+        hideProgressDialog()
+
+        showProgressDialog(resources.getString(R.string.please_wait))
+        FirestoreClass().getBoardDetails(this, mBoardDetails.documentId)
+    }
+
+    fun createTaskList(taskListName: String){
+        val task = Task(taskListName, FirestoreClass().getCurrentUserId())
+        mBoardDetails.taskList.add(0,task)
+        mBoardDetails.taskList.removeAt(mBoardDetails.taskList.size -1)
+
+        showProgressDialog(resources.getString(R.string.please_wait))
+
+        FirestoreClass().addUpdateTaskList(this,mBoardDetails)
+
+    }
 }
